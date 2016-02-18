09d34a62ecc21ef13aae0db5ed2f59eb1f750b36
seama107 <a8d4c3bab3dd47c89eb25e4486993de9@compute.c.sage-math-inc.internal>
2016-02-18 18:08:28 +0000

    Half the homework in this commit

---
+++ fit_pendulum_data.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+
+def retrieve_pendulum_data(filename = "pendulum.dat"):
+    """
+    Gets pendulum data from filename and outputs a 2d list of
+    output[0] = L and output[1] = T
+    """
+    L_list = []
+    T_list = []
+    with open(filename, 'r') as infile:
+        for line in infile:
+            data = line.split()
+            try:
+                L_list.append(float(data[0]))
+                T_list.append(float(data[1]))
+            except ValueError:
+                pass
+            
+    infile.close()
+    return [L_list, T_list]
+
+def pendulum_fit(L, T, degree):
+    """
+    Outputs the original list of lengths and a list of period variables as given by the fitted function T(L)
+    """
+    coeff = np.polyfit(L, T, degree)
+    p = np.poly1d(coeff)
+    print "Fitted Curve of degree {0}: ".format(degree)
+    print p
+    return [L,p(L)]
+
+
---
+++ midpoint_vec.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import numpy as np
+
+def midpointint(f, a, b, n):
+    """
+    Computes the Midpoint integration rule of f from a to b using a python for loop. 
+    """
+    running_sum = 0
+    for i in range(n):
+        running_sum += f(a + ((b-a)/float(n))*(i + .5) )
+    return running_sum * (b-a)/float(n)
+
+def return_midpoint_values(f, a, b, n):
+    """
+    Helper function for python and numpy sum
+    """
+    output = []
+    for i in range(n):
+        output.append(f(a + ((b-a)/float(n))*(i + .5) ))
+    return output
+
+def midpointint_python_sum(f, a, b, n):
+    """
+    Computes the Midpoint integration rule of f from a to b using the python sum function
+    """
+    midpoint_values = return_midpoint_values(f, a, b, n)
+    output_sum = sum(midpoint_values)
+    return output_sum * (b-a)/float(n)
+
+def midpointint_numpy_sum(f, a, b, n):
+    """
+    Computes the Midpoint integration rule of f from a to b using the numpy sum function
+    """
+    midpoint_values = return_midpoint_values(f, a, b, n)
+    output_sum = np.sum(midpoint_values)
+    return output_sum * (b-a)/float(n)
+
+def test_int_accuracy():
+    assert int(midpointint(function, 0, 3, 10)+.5) == 9
+
+def test_intpython_accuracy():
+    assert int(midpointint_python_sum(function, 0, 3, 10)+.5) == 9
+
+def test_intnumpy_accuracy():
+    assert int(midpointint_numpy_sum(function, 0, 3, 10)+.5) == 9
+
+def function(x):
+    return x*2
